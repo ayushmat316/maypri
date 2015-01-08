6e0474465737aef2681df7b5271e4d6c90796480
ayushmat316 <ayushmat316@gmail.com>
2015-01-08 05:31:10 -0500

    Major design updates & restaurant & vending pages

--- js/custom.js
+++ js/custom.js
@@ -1,24 +1,24 @@
 
 (function ($) {
-
-  /* Set fade interval for carousel images */  
+  
+  /*---- SET FADE INTERVAL FOR CAROUSEL IMAGES--- */  
     $(document).ready(function() {
         $('.carousel').carousel({interval: 3000});
     });
 
-  /*---- HEADER RESIZE ON SCROLL ANIMATION ---*/
-   /*
+  /*---- HEADER RESIZE ON SCROLL ANIMATION 
+   
     $(window).scroll(function() {
-        if ($(this).scrollTop() > 1){  
+        if ($(this).scrollTop() > 400){  
             $('.mainLogo').addClass("sticky");
         }
         else{
             $('.mainLogo').removeClass("sticky");
         }
-    });
-    */ 
-    
-    $("document").ready(function() {                                   
+    });---*/
+  
+ /*------ EASING SCROLL ON MAIN MAYRPI PAGE-----*/ 
+    $(document).ready(function() {                                   
         $('.us').click(function(){
             $('html, body').animate({
                 scrollTop: $(".heading-about").offset().top-100
@@ -33,6 +33,61 @@
 
     }); 
 
+/*----- EASING GO TO TOP -----
+   $(document).ready(function() {                                   
+        $('.goToTop').click(function(){
+            $('html, body').animate({
+                scrollTop: 0
+            }, 2000);                  
+        });
+    }); */
+
+/*----DISPLAY BACKGROUND INFO ON MAYPRI FOODS----*/
+   $(document).ready(function () {       
+       $('.bg').click(function() {
+            $('#background').show(800);
+            $('#restaurants').hide();
+            $('#vending').hide();
+            $('html, body').animate({
+                scrollTop: $("#background").offset().top-100
+            }, 2000);                                                                    
+
+        });
+    }); 
+
+/*---- DISPLAY RESTAURANT INFO ON MAYPRI FOODS-----*/
+    $(document).ready(function () {       
+       $('.rest').click(function() {
+            $('#background').hide();
+            $('#vending').hide();
+            $('#restaurants').show(800);
+            $('html, body').animate({
+                scrollTop: $("#restaurants").offset().top-150
+            }, 2000);                                                                    
+
+        });
+    }); 
+
+/*----- DISPLAY VENDING INFO ON MAYPRI FOODS---*/
+   $(document).ready(function () {       
+       $('.vend').click(function() {
+            $('#background').hide();
+            $('#restaurants').hide();
+            $('#vending').show(800);
+            $('html, body').animate({
+                scrollTop: $("#vending").offset().top-150
+            }, 2000);                                                                    
+
+        });
+    }); 
+
+   $(document).ready(function () {       
+       $('#dropdownImage').click(function() {
+            $('.logoOptions').toggle('fast');
+        });
+    }); 
+
+
     /*
     $(document).ready(function(){
         $("#profilePic1").click(function(){
@@ -44,7 +99,7 @@
     });
     */
 
-   $('header nav a').click(function() {
+/*   $('header nav a').click(function() {
         var $linkClicked = $(this).attr('href');
         document.location.hash = $linkClicked;
         if (!$(this).hasClass("active")) {
@@ -69,42 +124,44 @@
             break;
      }
 
-    /*	new gnMenu( document.getElementById( 'gn-menu' ) );
+*/
+
+    /*  new gnMenu( document.getElementById( 'gn-menu' ) );
 
 
-	//jQuery for page scrolling feature - requires jQuery Easing plugin
-	$(function() {
-		$('.listOptions li a').bind('click', function(event) {
-			var $anchor = $(this);
-			$('html, body').stop().animate({
-				scrollTop: $($anchor.attr('href')).offset().top
-			}, 1500, 'easeInOutExpo');
-			event.preventDefault();
-		});
-		$('a.scroll').bind('click', function(event) {
-			var $anchor = $(this);
-			$('html, body').stop().animate({
-				scrollTop: $($anchor.attr('href')).offset().top
-			}, 1500, 'easeInOutExpo');
-			event.preventDefault();
-		});
-	});
+    //jQuery for page scrolling feature - requires jQuery Easing plugin
+    $(function() {
+        $('.listOptions li a').bind('click', function(event) {
+            var $anchor = $(this);
+            $('html, body').stop().animate({
+                scrollTop: $($anchor.attr('href')).offset().top
+            }, 1500, 'easeInOutExpo');
+            event.preventDefault();
+        });
+        $('a.scroll').bind('click', function(event) {
+            var $anchor = $(this);
+            $('html, body').stop().animate({
+                scrollTop: $($anchor.attr('href')).offset().top
+            }, 1500, 'easeInOutExpo');
+            event.preventDefault();
+        });
+    });
 
-	
-	//nivo lightbox
-	$('.gallery-item a').nivoLightbox({
-		effect: 'fadeScale',                             // The effect to use when showing the lightbox
-		theme: 'default',                           // The lightbox theme to use
-		keyboardNav: true,                          // Enable/Disable keyboard navigation (left/right/escape)
-		clickOverlayToClose: true,                  // If false clicking the "close" button will be the only way to close the lightbox
-		onInit: function(){},                       // Callback when lightbox has loaded
-		beforeShowLightbox: function(){},           // Callback before the lightbox is shown
-		afterShowLightbox: function(lightbox){},    // Callback after the lightbox is shown
-		beforeHideLightbox: function(){},           // Callback before the lightbox is hidden
-		afterHideLightbox: function(){},            // Callback after the lightbox is hidden
-		onPrev: function(element){},                // Callback when the lightbox gallery goes to previous item
-		onNext: function(element){},                // Callback when the lightbox gallery goes to next item
-		errorMessage: 'The requested content cannot be loaded. Please try again later.' // Error message when content can't be loaded
-	}); */
+    
+    //nivo lightbox
+    $('.gallery-item a').nivoLightbox({
+        effect: 'fadeScale',                             // The effect to use when showing the lightbox
+        theme: 'default',                           // The lightbox theme to use
+        keyboardNav: true,                          // Enable/Disable keyboard navigation (left/right/escape)
+        clickOverlayToClose: true,                  // If false clicking the "close" button will be the only way to close the lightbox
+        onInit: function(){},                       // Callback when lightbox has loaded
+        beforeShowLightbox: function(){},           // Callback before the lightbox is shown
+        afterShowLightbox: function(lightbox){},    // Callback after the lightbox is shown
+        beforeHideLightbox: function(){},           // Callback before the lightbox is hidden
+        afterHideLightbox: function(){},            // Callback after the lightbox is hidden
+        onPrev: function(element){},                // Callback when the lightbox gallery goes to previous item
+        onNext: function(element){},                // Callback when the lightbox gallery goes to next item
+        errorMessage: 'The requested content cannot be loaded. Please try again later.' // Error message when content can't be loaded
+    }); */
 
 })(jQuery);
